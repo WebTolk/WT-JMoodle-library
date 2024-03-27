6b6a1b00680d7b2af6c03ae453db1d86f71059c3
Sergey Tolkachyov <musstudent@mail.ru>
2024-03-27 13:08:12 +0400

    Update version

--- lib_webtolk_jmoodle/src/Fields/CourseslistField.php
+++ lib_webtolk_jmoodle/src/Fields/CourseslistField.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @package       WT JMoodle Library
- * @version       1.0.3
+ * @version       1.1.0
  * @Author        Sergey Tolkachyov, https://web-tolk.ru
- * @сopyright     January 2024 Sergey Tolkachyov. All rights reserved.
+ * @сopyright (c) March 2024 Sergey Tolkachyov. All rights reserved.
  * @license       GNU/GPL3 http://www.gnu.org/licenses/gpl-3.0.html
  * @since         1.0.0
  */
